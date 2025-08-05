851ee5b290b8ecabad36b18fffd3459f4d0387a7
YOUR NAME <umarah.gul@gmail.com>
2025-08-05 12:31:39 +0500

    Created teacher class that inherits from person class

---
+++ teacher.rb
@@ -0,0 +1,17 @@
+require_relative 'person'
+class Teacher < Person
+ attr_accessor :specialization 
+
+
+ def initialize(age,specialization, name="unknown")
+  @specialization= specialization
+  super(age, name)
+ end
+
+
+ def can_use_services?
+  true
+ end
+
+end
+
