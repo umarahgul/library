925f27c14e9f1796bb014e45d35373c5271e9702
YOUR NAME <umarah.gul@gmail.com>
2025-08-05 11:18:06 +0500

    Created class student and inherited from person class

---
+++ student.rb
@@ -0,0 +1,15 @@
+class Student < Person
+
+ attr_accessor :classroom
+
+ def initialize(age,classroom, name="unknown", parent_permission=true)
+  super(age, name, parent_permission) # call person's constructor
+  @classroom= classroom
+ end
+
+ def play_hooky
+ print '"¯\(ツ)/¯"'
+ end
+end
+
+
